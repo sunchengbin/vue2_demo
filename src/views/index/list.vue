fea7b478c2a1f84766f3eb07d9c15fc3b41335dd
sunchengbin <sunchengbin@weidian.com>
2020-01-15 10:57:11 +0800

    fix:架构优化

---
+++ src/views/index/list.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="list-box">
+    <div>{{message}}</div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      message: 'this page is list'
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .list-box {
+    font-size: 100px;
+  }
+</style>
